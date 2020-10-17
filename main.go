3ef81817d129e8b22b1ceee2b8ffc89a62880c47
Dan Hartman <dan.h@rtman.org>
2020-10-17 16:27:59 -0500

    Create hello world blinking led

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"time"
+    "os"
+    "log"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/firmata"
+)
+
+func main() {
+    // environment variable assigns port for app to listen on
+	port := os.Getenv("DEV_PORT")
+	if port == "" {
+		log.Fatal("Env Var DEV_PORT must be set")
+	}
+	firmataAdaptor := firmata.NewAdaptor(port)
+	led := gpio.NewLedDriver(firmataAdaptor, "13")
+
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("bot",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{led},
+		work,
+	)
+
+	robot.Start()
+}
